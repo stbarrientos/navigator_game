75e9073c31d8eea08eb71d70211ce1107eb7586d
Sam Barrientos <stbarrientos@gmail.com>
2014-07-03 09:31:54 -0400

    Adding practic_stuff.rb

---
+++ practice_stuff.rb
@@ -0,0 +1,54 @@
+###### Classes ######
+
+class User
+	@@users_hash = {}
+	def initialize(name, password)
+		@name = name
+		@password = password
+		@@users_hash[@name] = @password
+	end
+
+	def self.show_users
+		@@users_hash.each { |name, password| puts "Name: #{name}" }
+	end
+end
+
+class Apartment
+	@@apartments_hash = {}
+	def initialize(address, price, size = 1, city = "DC")
+		@address = address
+		@price = price
+		@size = size
+		@city = city
+		@@apartments_hash[@address] = @price
+	end
+
+	def self.show_apartments
+		@@apartments_hash.each { |address, price| puts "Apartment at: #{address}, for #{price}"}
+	end
+
+	def self.under(max_price)
+		puts "These apartments are all less than $#{max_price}"
+		@@apartments_hash.each { |address, price| puts price < max_price ? "#{address} @ $#{price}" : nil }
+	end
+
+	def self.print(file1, max_price = 2000)
+		File.open(file1, 'w') { |f| f.write(Apartment.under(max_price)) }
+	end
+end
+
+###### Procs ########
+
+create_users = Proc.new { |x| User.new("user#{x}", "pass{x}")}
+create_apartments = Proc.new { |x| Apartment.new("#{x} Street NW", x * 500) }
+
+###### Lambdas #######
+
+###### use_cases ########
+
+5.times(&create_users)
+User.show_users
+
+5.times(&create_apartments)
+Apartment.under(2000)
+Apartment.print("new.txt")
