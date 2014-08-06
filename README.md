4eba096c1e018411fb2d9791283b07d2e9586524
stbarrientos <stbarrientos@gmail.com>
2014-08-06 13:30:26 -0400

    8-6-14, pre collaboration from classmates

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 Hello world! This is my first addition!
-This is the second addition in a new branch
+
+Alright guys so the navigator.rb file is the one I was talking about. It's gonna be hard to explain on here so just ask me in person. Thanks for helping out! This will be great git practice too.
--- navigator.rb
+++ navigator.rb
@@ -15,6 +15,7 @@ class World
 				@world_grid[x][y] = "."
 			end
 		end
+		return "done"
 	end
 
 	def show_world
@@ -22,32 +23,31 @@ class World
 			x.each { |y| print "#{y} " }
 			puts ""
 		end
+		return "done"
 	end
 
 	def edit_world(row, col, val)
 		@world_grid[row][col] = val
 	end
-
 end
 
 class Person
 
 	def initialize(world, row, col)
+		@world = world
 		@row = row
 		@col = col
-		world.edit_world(@row,@col, "@")
+		@world.edit_world(@row,@col, "@")
 	end
 end
 
 class Destination
 
-	@@destination_count
-	attr_accessor :destination_position
-
 	def initialize(world,row, col)
+		@world = world
 		@row = row
 		@col = col
-		world.edit_world(@row,@col, "*")
+		@world.edit_world(@row,@col, "*")
 	end
 end
 
@@ -56,21 +56,26 @@ class Obstruction
 	attr_accessor :obstruction_position
 
 	def initialize(world, row, col)
+		@world = world
 		@row = row
 		@col = col
-		world.edit_world(@row, @col, "!")
-		@obstruction_position = "#{@row},#{@col}"
-
+		@world.edit_world(@row, @col, "!")
+		@obstruction_position = [@row,@col]
 	end
 end
 
-def gps(world)
+# mine = World.new(4,5)
+# obi = Obstruction.new(mine,1,2)
+# me = Person.new(mine,1,4)
+# dest = Destination.new(mine,1,0)
+# mine.show_world
 
-end
 
-mine = World.new(4,5)
-obi = Obstruction.new(mine,1,2)
-me = Person.new(mine,1,4)
-dest = Destination.new(mine,1,0)
-mine.show_world
-puts obi.obstruction_position
+# puts obi.obstruction_position
+puts "welcome to the navigator. Start with World.new, Obstruction.new, Person.new, and Destination.new. See your world with show world"
+
+
+
+
+
+
