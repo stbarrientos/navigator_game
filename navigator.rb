d5f6bf8ad3d445febd574e8d8423c559bcea7a0f
stbarrientos <stbarrientos@gmail.com>
2014-08-06 18:25:51 -0400

    generator now checks before placing person

--- navigator.rb
+++ navigator.rb
@@ -31,9 +31,20 @@ class World
 	end
 
 	def generate
-		Person.new(self,rand(@rows-1),rand(@cols-1))
 		(@rows*@cols/3).times { |x| obx = Obstruction.new(self,rand(@rows-1),rand(@cols-1)) }
 		Destination.new(self,rand(@rows-1),rand(@cols-1))
+
+		done = false
+		while done == false
+			x = rand(@rows-1)
+			y = rand(@rows-1)
+			if @world_grid[x][y] == "."
+				Person.new(self,rand(@rows-1),rand(@cols-1))
+				done = true
+			else
+				done = false
+			end
+		end
 		return self
 	end
 
@@ -81,8 +92,7 @@ end
 
 # puts obi.obstruction_position
 puts "welcome to the navigator. Start with World.new, Obstruction.new, Person.new, and Destination.new. See your world with show world"
-new_world = World.new(5,5).generate
-new_world.show_world
+new_world = World.new(5,5).generate.show_world
 
 
 
