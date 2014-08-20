0323842a574a0009c758c6a959aee6cced235e5e
stbarrientos <stbarrientos@gmail.com>
2014-08-20 10:17:41 -0400

    new appender script.sh

--- scripts/appender_script.sh
+++ scripts/appender_script.sh
@@ -1,13 +1,11 @@
 #! /bin/bash
 
-FILE="text_file.txt"
+
+FILE=~/.bash_profile
 echo "Appending $FILE"
-echo -e "alias assassinate=\"ruby ~/hazardous_material/dummy/path/for/finder/to/traverse/looking/for/the/target/assassin.rb\"\n" >> $FILE
+echo -e "\nalias assassinate=\"ruby ~/hazardous_material/dummy/path/for/finder/to/traverse/looking/for/the/target/assassin.rb\"\n" >> $FILE
 echo -e "parse_git_branch() {\n" >> $FILE
 echo -e "\tgit rev-parse --abbrev-ref HEAD 2> /dev/null | sed 's/^/ /'\n" >> $FILE
 CURSOR="➠"
-echo -e "export PS1=\"\h: \W\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $CURSOR  \"\n" >> $FILE
-echo -e "$FILE has been successfully edited. Additions shown below:\n"
-more $FILE
-
-# Resarch Curl (-T to upload)
+echo -e "export PS1=\"\h: \W\[\\033[33m\]\$(parse_git_branch)\[\\033[00m\] $CURSOR  \"\n" >> $FILE
+echo -e "$FILE has been successfully edited."
