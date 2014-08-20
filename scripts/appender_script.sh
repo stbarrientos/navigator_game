61d1545a90d672e9bbcca53108b67568c329c885
stbarrientos <stbarrientos@gmail.com>
2014-08-20 10:24:32 -0400

    mighthave fixed escaping problem

--- scripts/appender_script.sh
+++ scripts/appender_script.sh
@@ -7,5 +7,5 @@ echo -e "\nalias assassinate=\"ruby ~/hazardous_material/dummy/path/for/finder/t
 echo -e "parse_git_branch() {\n" >> $FILE
 echo -e "\tgit rev-parse --abbrev-ref HEAD 2> /dev/null | sed 's/^/ /'\n" >> $FILE
 CURSOR="➠"
-echo -e "export PS1=\"\h: \W\[\\033[33m\]\$(parse_git_branch)\[\\033[00m\] $CURSOR  \"\n" >> $FILE
+echo -e "export PS1=\"\\h: \\W\[\\033[33m\\]\$(parse_git_branch)\\[\\033[00m\\] $CURSOR  \"\n" >> $FILE
 echo -e "$FILE has been successfully edited."
